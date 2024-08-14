3734897782109615fd4a3e67f82a60fb52f3b92d
gamma63 <174414990+gamma63@users.noreply.github.com>
2024-08-14 21:03:14 +0300

    epic glibc stuff 3

--- limits.h
+++ limits.h
@@ -2,10 +2,25 @@
 
 #define _LITTLE_ENDIAN
 
+/* Copyright (C) 1991-2024 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
 #define UCHAR_MAX 255
 #define CHAR_BIT 8
-
-// GPL License below :P (https://github.com/bminor/glibc/blob/master/include/limits.h)
 
 /* We don't have #include_next.
    Define ANSI <limits.h> for standard 32-bit words.  */
